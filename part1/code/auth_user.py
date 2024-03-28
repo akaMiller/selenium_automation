5b6658f522a3dac0f090cf4391beeab18f093af4
akaMiller <millerpage@gmail.com>
2024-03-28 14:26:32 +0100

    Update case 05

--- part1/code/auth_user.py
+++ part1/code/auth_user.py
@@ -1,11 +1,14 @@
 # Авторизация пользователя по логину и паролю
 
-from selenium import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+# browser = webdriver.Chrome()
 
 def auth_user(browser):
     browser.get('https://www.saucedemo.com/v1/')
+    #Данные для авторизации доступны в коде, т.к. они открыто размещены на страницы входа :)
     browser.find_element(By.XPATH, '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    browser.implicitly_wait(0.5)
+    # browser.implicitly_wait(0.5)
--- part1/code/test_case05_add_products_from_card_product.py
+++ part1/code/test_case05_add_products_from_card_product.py
@@ -1,1 +1,25 @@
 ## Добавление товара в корзину из карточки товара
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import auth_user
+
+
+browser = webdriver.Chrome()
+
+
+def test_add_product_from_card_product():
+    auth_user.auth_user(browser)
+    # Можем получить ссылку на карточку продукта, чтобы потом ее сравнить с адресом продукта
+    # elem = browser.find_element(By.CSS_SELECTOR,
+    #                             '.inventory_item:first-of-type .inventory_item_label [href]').get_attribute('href')
+    browser.find_element(By.CSS_SELECTOR,
+                         '.inventory_item:first-of-type .inventory_item_name:first-of-type').click()
+    # Можем проверить, что карточка продукта совпадает с ссылкой в продукте
+    # assert browser.current_url == elem , 'Адрес страницы продукта не совпадает с выбранным продуктом!!!'
+    browser.find_element(By.CSS_SELECTOR, "button[class='btn_primary btn_inventory']").click()
+    cart = browser.find_element(By.CSS_SELECTOR, "span[class='fa-layers-counter shopping_cart_badge']")
+    assert cart.text == '1', 'В корзине не один товара!!!'
+    browser.quit()
+
