1d1b2246d3a84bae0d5799ba7a6193f201d592c6
akaMiller <millerpage@gmail.com>
2024-03-28 15:28:27 +0100

    Update case 06

---
+++ part1/code/test_case06_delete_products_from_product_card.py
@@ -0,0 +1,18 @@
+# Удаляем продукт из карточки товара
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import auth_user
+
+browser = webdriver.Chrome()
+
+
+def test_delete_product_from_product_card():
+    auth_user.auth_user(browser)
+    browser.find_element(By.CSS_SELECTOR,
+                         '.inventory_item:first-of-type .inventory_item_name:first-of-type').click()
+    browser.find_element(By.CSS_SELECTOR, "button[class='btn_primary btn_inventory']").click()
+    browser.find_element(By.CSS_SELECTOR, "button[class='btn_secondary btn_inventory']").click()
+    assert 'fa-layers-counter shopping_cart_badge' not in browser.page_source
+    browser.quit()
