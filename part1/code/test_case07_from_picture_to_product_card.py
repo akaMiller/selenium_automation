f731605ceb15c327a7793caa7e89494374cb4946
akaMiller <millerpage@gmail.com>
2024-03-28 17:21:24 +0100

    Update case 07-08

--- part1/code/test_case07_from_picture_to_product_card.py
+++ part1/code/test_case07_from_picture_to_product_card.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 import pytest
 import auth_user
 
-
 browser = webdriver.Chrome()
 
 
@@ -13,10 +12,13 @@ def test_from_picture_to_product_card():
     auth_user.auth_user(browser)
     # Можем получить ссылку из имени на карточку продукта, чтобы потом ее сравнить с адресом продукта
     elem = browser.find_element(By.CSS_SELECTOR,
-                                '.inventory_item:first-of-type .inventory_item_label [href]').get_attribute('href')
+                                '.inventory_item:first-of-type div[class="inventory_item_img"] [href]').get_attribute(
+        'href')
     # Кликаем на картинку первого продукта
     browser.find_element(By.CSS_SELECTOR,
                          '.inventory_item:first-of-type div[class="inventory_item_img"]').click()
     # Можем проверить, что карточка продукта совпадает с ссылкой в продукте
     assert browser.current_url == elem, 'Адрес страницы продукта не совпадает с выбранным продуктом!!!'
+    # Проверяем, что перешли на новую страницу
+    assert browser.current_url != 'https://www.saucedemo.com/v1/inventory.html', 'URL не соотвествует ожидаемому'
     browser.quit()
---
+++ part1/code/test_case08_from_name_to_product_card.py
@@ -0,0 +1,24 @@
+#Кейс 8: Успешный переход к карточке товара после клика на название товара
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import auth_user
+
+
+browser = webdriver.Chrome()
+
+
+def test_from_name_to_product_card():
+    auth_user.auth_user(browser)
+    #Можем получить ссылку на карточку продукта, чтобы потом ее сравнить с адресом продукта
+    elem = browser.find_element(By.CSS_SELECTOR,
+                                '.inventory_item:first-of-type .inventory_item_label [href]').get_attribute('href')
+    #Ищем имя продукта и кликаем по нему
+    browser.find_element(By.CSS_SELECTOR,
+                         '.inventory_item:first-of-type .inventory_item_name:first-of-type').click()
+    # Можем проверить, что карточка продукта совпадает с ссылкой в продукте
+    assert browser.current_url == elem , 'Адрес страницы продукта не совпадает с выбранным продуктом!!!'
+    #Проверяем, что перешли на новую страницу
+    assert browser.current_url != 'https://www.saucedemo.com/v1/inventory.html', 'URL не соотвествует ожидаемому'
+    browser.quit()
