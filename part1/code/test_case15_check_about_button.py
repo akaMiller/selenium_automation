08ed62e6b3dcefd0aece25f364af3fd112820963
akaMiller <millerpage@gmail.com>
2024-03-31 14:45:40 +0200

    Update case 15

---
+++ part1/code/test_case15_check_about_button.py
@@ -0,0 +1,22 @@
+#Проверка работоспособности кнопки "About" в меню
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import pytest
+import auth_user
+
+browser = webdriver.Chrome()
+
+
+def test_check_about_button():
+    auth_user.auth_user(browser)
+    #Вызываем меню в левой верхнем углу
+    browser.find_element(By.CSS_SELECTOR, '.bm-burger-button').click()
+    #Ждем
+    browser.implicitly_wait(0.5)
+    #Кликаем на кнопку About
+    browser.find_element(By.CSS_SELECTOR, '#about_sidebar_link').click()
+    # Сравниваем страницу
+    assert browser.current_url == 'https://saucelabs.com/', 'URL не соотвествует ожидаемому'
+    browser.quit()
--- part1/user-case/Case15_check_about_button.md
+++ part1/user-case/Case15_check_about_button.md
@@ -1,3 +1,11 @@
 Кейс 15: Проверка работоспособности кнопки "About" в меню
 ---
 
+1. Открыть URL https://www.saucedemo.com/v1/
+2. Ввести 'standard_user' в поле Username
+3. Ввести 'secret_sauce' в поле Password
+4. Кликнуть 'Login'
+5. Вызвать меню в левом верхнем углу.
+6. Нажать About
+
+[Автоматизированный кейс 15](https://github.com/akaMiller/selenium_automation/blob/main/part1/code/test_case15_check_about_button.py)
