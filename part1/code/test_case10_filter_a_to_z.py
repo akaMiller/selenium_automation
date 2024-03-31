f45c420fd3a5cb3c1d54fea6ae94e52b59970b26
akaMiller <millerpage@gmail.com>
2024-03-31 13:50:18 +0200

    Update case 10

---
+++ part1/code/test_case10_filter_a_to_z.py
@@ -0,0 +1,23 @@
+#Проверка работоспособности фильтра (A to Z)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import pytest
+import auth_user
+
+
+browser = webdriver.Chrome()
+def test_filter_a_to_a():
+    auth_user.auth_user(browser)
+    #сортируем по az
+    dropdown = Select(browser.find_element(By.CSS_SELECTOR, "select[class='product_sort_container']"))
+    dropdown.select_by_value("az")
+    #Получаем имена продуктов на странице
+    products = browser.find_elements(By.CSS_SELECTOR, 'div.inventory_item_name')
+    products_name = []
+    for product in products:
+        products_name.append(product.text)
+    #Сравниваем с учетом сортировки
+    assert products_name == sorted(products_name, reverse=False), 'Фильтр AZ не работает!!!'
+    browser.quit()
--- part1/user-case/Case10_filter_a_to_z.md
+++ part1/user-case/Case10_filter_a_to_z.md
@@ -1,3 +1,10 @@
 Кейс 10: Проверка работоспособности фильтра (A to Z)
 ---
 
+1. Открыть URL https://www.saucedemo.com/v1/
+2. Ввести 'standard_user' в поле Username
+3. Ввести 'secret_sauce' в поле Password
+4. Кликнуть 'Login'
+5. На странице отсортировать список товаров A-Z по кнопке сортировки в правом верхнем углу
+
+[Автоматизированный кейс 10](https://github.com/akaMiller/selenium_automation/blob/main/part1/code/test_case10_filter_a_to_z.py)
